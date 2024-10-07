33d0e6780987d55e0019accafb59bb8326b5f0eb
blazej686 <144096019+blazej686@users.noreply.github.com>
2024-10-06 20:04:04 -0600

    Cleaning and testing server  side

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,10 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) UNIQUE COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8mb4 COMMENT '';
+-- Active: 1705429522848@@54.214.151.71@3306@GeoTracker
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture',
+    coverImg VARCHAR(255)
+) default charset utf8 COMMENT '';
