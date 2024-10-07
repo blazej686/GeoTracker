53047408a864c3ccf1685262bd7829c4ea210d54
blazej686 <144096019+blazej686@users.noreply.github.com>
2024-10-06 19:20:40 -0600

    init

---
+++ server/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace GeoTracker.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
